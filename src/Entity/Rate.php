b0da9f8d3721ffb50ce9a726d028ee872277e9a7
Yaroslav Harbolinskyi <yaroslav.artistsoft@gmail.com>
2020-08-09 19:58:16 +0300

    Added Rate entity and repository.

---
+++ src/Entity/Rate.php
@@ -0,0 +1,93 @@
+<?php declare(strict_types = 1);
+
+namespace App\Entity;
+
+use App\Repository\RateRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RateRepository::class)
+ * @ORM\Table(name="rates")
+ */
+class Rate
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="base_currency", type="string", length=3)
+     */
+    private $baseCurrency;
+
+    /**
+     * @ORM\Column(name="target_currency", type="string", length=3)
+     */
+    private $targetCurrency;
+
+    /**
+     * @ORM\Column(name="exchange_rate", type="decimal", precision=13, scale=8)
+     */
+    private $exchangeRate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getBaseCurrency(): ?string
+    {
+        return $this->baseCurrency;
+    }
+
+    public function setBaseCurrency(string $baseCurrency): self
+    {
+        $this->baseCurrency = $baseCurrency;
+
+        return $this;
+    }
+
+    public function getTargetCurrency(): ?string
+    {
+        return $this->targetCurrency;
+    }
+
+    public function setTargetCurrency(string $targetCurrency): self
+    {
+        $this->targetCurrency = $targetCurrency;
+
+        return $this;
+    }
+
+    public function getExchangeRate(): ?string
+    {
+        return $this->exchangeRate;
+    }
+
+    public function setExchangeRate(string $exchangeRate): self
+    {
+        $this->exchangeRate = $exchangeRate;
+
+        return $this;
+    }
+
+    public function getUpdated(): ?\DateTimeInterface
+    {
+        return $this->updated;
+    }
+
+    public function setUpdated(\DateTimeInterface $updated): self
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+}
---
+++ src/Repository/RateRepository.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace App\Repository;
+
+use App\Entity\Rate;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class RateRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Rate::class);
+    }
+}

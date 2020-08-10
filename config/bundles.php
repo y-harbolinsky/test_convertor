617b2828a594d5ff95069f2f25ce2bd05abd5611
Yaroslav Harbolinskyi <yaroslav.artistsoft@gmail.com>
2020-08-10 14:02:47 +0300

    Added framework-extra-bundle and twig bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
